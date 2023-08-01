c84c0feab34099c6540210d82f83c69dbf7df5ea
Jatin Tank <jatin.tank.official@gmail.com>
2023-08-01 16:30:10 +0530

    Check array is sorted

--- Arrays/checkSortedArray.cpp
+++ Arrays/checkSortedArray.cpp
@@ -1,7 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
+bool isSorted(vector<int> &arr)
+{
+    if (arr.size() == 1)
+        return true;
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] > arr[i + 1])
+            return false;
+        return true;
+    }
+}
 int main()
 {
-    cout << "Hello World\n";
+    vector<int> arr = {1, 20, 30};
+    (isSorted(arr) == 1) ? cout << "Sorted" : cout << "Not Sorted\n";
     return 0;
 }
