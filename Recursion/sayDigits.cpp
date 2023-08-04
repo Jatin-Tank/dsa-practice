4d37166d0b5d0b7d7179107e9c0e4388d50cd0c8
Jatin Tank <jatin.tank.official@gmail.com>
2023-08-04 19:07:18 +0530

    Say Digits

--- Recursion/sayDigits.cpp
+++ Recursion/sayDigits.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 using namespace std;
-void sayDigits(int n)
+void sayDigits(int n, string arr[])
 {
-    string arr[] = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+
     // Base Case
     if (n == 0)
         return;
 
-    sayDigits(n / 10);
+    sayDigits(n / 10, arr);
     cout << arr[n % 10] << " ";
 }
 int main()
 {
+    string arr[] = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
     int n = 412;
-    sayDigits(n);
+    sayDigits(n, arr);
     return 0;
 }
