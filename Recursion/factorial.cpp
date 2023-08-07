987ecaa76bd9ca9c3fcef7463a0567a3e69ce3fa
Jatin Tank <jatin.tank.official@gmail.com>
2023-08-07 17:57:51 +0530

    Factorial optimized

--- Recursion/factorial.cpp
+++ Recursion/factorial.cpp
@@ -2,11 +2,9 @@
 using namespace std;
 int factorial(int n)
 {
-    // Base case
     if (n == 0)
         return 1;
     int s = factorial(n - 1);
-    // cout << s << endl;
     return n * s;
 }
 int main()
